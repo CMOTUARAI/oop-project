880a8c34ca783bc1433573044cb068d34ec5fa3e
Tan <tan2tanpavidh@gmail.com>
2026-01-18 19:58:30 +0700

    fr final commit

--- date.py
+++ date.py
@@ -41,3 +41,38 @@ except :
 
 print("day of year:",doy,end=" ,")
 print("is_leap:",leap_pa)
+
+def day_in_year(year):
+    if(is_leap(year)):
+        return 366
+    else:
+        return 365
+
+def date_diff(date1,date2):
+    date1 = list(map(int, date1.split("-")))
+    date2 = list(map(int, date2.split("-")))
+    doy1 = day_of_year(date1[0], date1[1] ,date1[2])
+    doy2 = day_of_year(date2[0], date2[1] ,date2[2])
+    totalnday = 0
+    for x in range(date1[2]+1,date2[2]):
+        totalnday += day_in_year(x)
+    if date1[2]==date2[2]:
+        dur = doy2 - doy1 + 1
+    else:
+        dur = (day_in_year(date1[2])-doy1) + totalnday + doy2+1
+
+    if date1[1] < 1 or date1[1] > 12 or date2[1] < 1 or date2[1] > 12:
+        print("Invalid")    
+    elif date1[0] < 1 or date1[0] > day_in_month[date1[1]] or date2[0] < 1 or date2[0] > day_in_month[date2[1]]:
+        print("Invalid")   
+    elif (date1[1] == 2 and date1[0] == 29 and not is_leap(date1[2])) or (date2[1] == 2 and date2[0] == 29 and not is_leap(date2[2])):
+        print("Invalid")
+    else:
+        print(dur)
+
+date = input("Enter Input: ").split(",")
+date1 = date[0]
+date2 = date[1]
+date_diff(date1,date2)
+
+print("love you all jubu jubu")
